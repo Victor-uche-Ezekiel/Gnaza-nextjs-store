7a10e128e2de6270031f6afdb1fb90de3a0e3eb8
Victor-Ezekiel-blinks <victoruche3022@gmail.com>
2024-02-09 12:53:53 +0100

    added api url

--- components/FeaturedProducts.tsx
+++ components/FeaturedProducts.tsx
@@ -5,26 +5,12 @@ import Product from "./Product";
 import { cn } from "./helpers";
 import { SingleProduct } from "@/libs/types";
 import { resolve } from "path";
+import { api_url, fetchFeaturedProducts } from "@/libs/httpcalls";
 
 type Props = {
   fProductsText: string;
   className?: string;
   pclassName?: string;
-};
-
-const fetchFeaturedProducts = async () => {
-  try {
-    const res = await fetch(`http://localhost:3000/api/featuredProducts`, {
-      // cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("failed to fetch data");
-    }
-    return res.json();
-  } catch (error) {
-    console.log("error", error);
-  }
 };
 
 const FeaturedProducts = async ({
@@ -34,32 +20,34 @@ const FeaturedProducts = async ({
 }: Props) => {
   const allProducts: SingleProduct[] = await fetchFeaturedProducts();
 
-  return (
-    <div className={`${cn("mb", pclassName)}`}>
-      <h1 className="sub__section">{fProductsText}</h1>
+  // return (
+  //   <div className={`${cn("mb", pclassName)}`}>
+  //     <h1 className="sub__section">{fProductsText}</h1>
 
-      <div className={`${cn("scenter2 prXl:gap-[2rem]", className)}`}>
-        {allProducts.map((product) => {
-          const { productName, _id, productImage, price } = product;
+  //     <div className={`${cn("scenter2 prXl:gap-[2rem]", className)}`}>
+  //       {allProducts.map((product) => {
+  //         const { productName, _id, productImage, price } = product;
 
-          return (
-            <Suspense key={_id} fallback={<h1>featured products</h1>}>
-              <Product
-                key={_id}
-                name={productName}
-                price={price}
-                id={_id}
-                productImage={productImage}
-                className={{
-                  img: "h-[30rem] prXl:h-[26rem] prLg:h-[19rem] prMd:h-[16rem] prSm:h-[28rem]",
-                }}
-              />
-            </Suspense>
-          );
-        })}
-      </div>
-    </div>
-  );
+  //         return (
+  //           <Suspense key={_id} fallback={<h1>featured products</h1>}>
+  //             <Product
+  //               key={_id}
+  //               name={productName}
+  //               price={price}
+  //               id={_id}
+  //               productImage={productImage}
+  //               className={{
+  //                 img: "h-[30rem] prXl:h-[26rem] prLg:h-[19rem] prMd:h-[16rem] prSm:h-[28rem]",
+  //               }}
+  //             />
+  //           </Suspense>
+  //         );
+  //       })}
+  //     </div>
+  //   </div>
+  // );
+
+  return <h1>Featured Products Page</h1>;
 };
 
 export default FeaturedProducts;
--- libs/httpcalls.ts
+++ libs/httpcalls.ts
@@ -1,4 +1,8 @@
 // fetch all products
+export const api_url = process.env.API_URL;
+export const public_api_url = process.env.NEXT_PUBLIC_API_URL;
+
+// All Products Fetch Function
 export const fetchAllProducts = async (
   e: string | null,
   r: string | null,
@@ -14,14 +18,11 @@ export const fetchAllProducts = async (
   try {
     setIsLoading(true);
     const res = await fetch(
-      `http://localhost:3000/api/products?e=${e}&r=${r}&n=${n}&b=${b}&a=${a}&page=${
+      `${public_api_url}/api/products?e=${e}&r=${r}&n=${n}&b=${b}&a=${a}&page=${
         p || 1
       }&sort=${sort}`,
       {
         method: "GET",
-        // cache:''
-        // cache: "no-cache",
-        // cache: "force-cache",
       }
     );
 
@@ -31,6 +32,35 @@ export const fetchAllProducts = async (
     const data = await res.json();
     setIsLoading(false);
     setAllProducts(data);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// Featured Products Fetch Function
+export const fetchFeaturedProducts = async () => {
+  try {
+    const res = await fetch(`${api_url}/api/featuredProducts`, {
+      cache: "no-cache",
+    });
+
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const fetchSingleProducts = async (id: string) => {
+  try {
+    const res = await fetch(`${api_url}/api/products/${id}`);
+
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+    return res.json();
   } catch (error) {
     console.log("error", error);
   }
