901f79aac6e08c01bc327520a3489a1828f4130a
Victor-Ezekiel-blinks <victoruche3022@gmail.com>
2024-03-23 08:30:43 +0100

    comment singleModel

--- components/SingleProductModel.tsx
+++ components/SingleProductModel.tsx
@@ -1,19 +1,18 @@
-/* eslint-disable */
-import React from "react";
-import Quantity from "./Quantity";
-import AddToCart from "./AddToCart";
-import { ESLINT_DEFAULT_DIRS } from "next/dist/lib/constants";
+// /* eslint-disable */
+// import React from "react";
+// import Quantity from "./Quantity";
+// import AddToCart from "./AddToCart";
 
-type Props = {};
+// type Props = {};
 
-function SingleProductModel({}: Props) {
-  return (
-    <div>
-      <Quantity />
-      <AddToCart buttonText="add to cart" className="" />
-      <AddToCart buttonText="buy now" className="" />
-    </div>
-  );
-}
+// function SingleProductModel({}: Props) {
+//   return (
+//     <div>
+//       <Quantity />
+//       <AddToCart buttonText="add to cart" className="" />
+//       <AddToCart buttonText="buy now" className="" />
+//     </div>
+//   );
+// }
 
-export default SingleProductModel;
+// export default SingleProductModel;
